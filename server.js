f30baf7267c01d9b19e8c1c832c716b977e0c0e7
Bhavya Verma <me.bhavyaverma111@gmail.com>
2023-02-08 19:41:01 +0530

    deploy

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  5000,
+  PORT,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
